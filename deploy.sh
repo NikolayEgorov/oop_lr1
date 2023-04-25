f5e27bd078539ca333d3cb4e76ef69576db61b47
Mykola Yehorov <egorov.n@vuso.ua>
2023-04-25 10:39:09 +0300

    deploy update

--- deploy.sh
+++ deploy.sh
@@ -3,10 +3,11 @@ ssh-add ~/.ssh/id_rsa
 
 echo 'updating project...'
 
-cd /home/mykola/dotNetProjects
+PROJECTS_PATH="/home/mykola/dotNetProjects"
 
-ssh -F ~/.ssh/config vm rm -rf /home/mykola/dotnetProjects/labrob1/*
-scp -F ~/.ssh/config -r labrob1/* vm:/home/mykola/dotnetProjects/labrob1/
+cd $PROJECTS_PATH
+
+ssh -F ~/.ssh/config vm rm -rf $PROJECTS_PATH/labrob1/*
+scp -F ~/.ssh/config -r labrob1/* vm:$PROJECTS_PATH/labrob1/
 
 ssh-agent -k
-
